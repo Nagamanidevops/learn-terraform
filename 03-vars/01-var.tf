8cb1f2b2789b815b29b8de1d2c97ee991ca3df10
nagamani <nagamanichavaaws@gmail.com>
2023-02-15 08:20:14 +0000

    vartypes

--- 03-vars/01-var.tf
+++ 03-vars/01-var.tf
@@ -5,3 +5,27 @@ variable "sample"{
 output "sample"{
   value = var.sample
     }
+
+variable "sample2"{
+    default = "string"
+}
+
+output "sample2"{
+    value = var.sample2
+}
+
+variable "sample3"{
+    default = 10
+}
+
+output "sample2"{
+    value = 10
+}
+
+variable "sample4"{
+    default = true
+}
+
+output "sample4"{
+    value = var.sample4
+}
