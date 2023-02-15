3bbd788d5b59d11755aa785fc117e1ece1589dbb
nagamani <nagamanichavaaws@gmail.com>
2023-02-15 10:02:48 +0000

    resources

---
+++ 04-resources/sample.tf
@@ -0,0 +1,23 @@
+provider "aws"{
+    region = "us-east1-1"
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+resource "aws_instance" "test" {
+    
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos8.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
