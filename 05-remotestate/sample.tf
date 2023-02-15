2bfc564b021185d3420ce78b760be45688582886
nagamani <nagamanichavaaws@gmail.com>
2023-02-15 10:42:38 +0000

    added bucket

---
+++ 05-remotestate/sample.tf
@@ -0,0 +1,27 @@
+provider "aws"{
+    region = "us-east-1"
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos8.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-d70"
+    key    = "05-remotestate/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
