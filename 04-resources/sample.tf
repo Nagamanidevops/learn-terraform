e4df12e4bfbe6f740dbd66d713f46185144962d9
nagamani <nagamanichavaaws@gmail.com>
2023-02-15 10:05:32 +0000

    del

--- 04-resources/sample.tf
+++ 04-resources/sample.tf
@@ -9,10 +9,6 @@ data "aws_ami" "centos8" {
 }
 
 
-resource "aws_instance" "test" {
-    
-}
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.centos8.id
   instance_type = "t3.micro"
