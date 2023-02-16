de8189902fcf5c45ffe06cb539adb39e07ebbdb1
nagamani <nagamanichavaaws@gmail.com>
2023-02-16 11:29:19 +0000

    aa

---
+++ 08-loops/count.1/main.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "web" {
+  count = length(var.componet)
+  ami           = data.aws_ami.centos8.id
+  instance_type = "t3.micro"
+  tags = {
+    Name = var.componet[count.index]
+  }
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+output "publicip"{
+   value = aws_instance.web.*.public_ip
+}
+
+variable "componet"{
+    default  = {
+      cart = {
+      name = "cart",
+      instance_type = "t3.micro"
+    }
+      cart = {
+      name = "catalog",
+      instance_type = "t3.small"
+    
+}
