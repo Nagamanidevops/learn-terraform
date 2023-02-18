8494aabebb3412afe44077e3c0748569dcef5cd0
nagamani <nagamanichavaaws@gmail.com>
2023-02-18 16:07:22 +0000

    a

--- 05-remotestate/sample.tf
+++ 05-remotestate/sample.tf
@@ -10,11 +10,11 @@ data "aws_ami" "centos8" {
 
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.centos8.id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
-  tags = {
+  tags   = {
     Name = "HelloWorld"
   }
 }
